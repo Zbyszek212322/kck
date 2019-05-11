4e6fa48e79501aa3f5ade4872c35bdb0dbc12cb4
Zbyszek212322 <zbyszrad@poczta.wp.pl>
2019-05-11 17:55:25 +0200

    youtube

--- src/background.js
+++ src/background.js
@@ -20,7 +20,7 @@ function createWindow () {
   var mainScreen = screenElectron.getPrimaryDisplay();
   const {width, height} = mainScreen.bounds
   // Create the browser window.
-  win = new BrowserWindow({ width: width/4, height, x:0, y:0, 
+  win = new BrowserWindow({ width: width/3, height, x:0, y:0, 
   webPreferences: {
     nodeIntegration: true,
      devTools: false
